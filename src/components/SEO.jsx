bf3e20de240e91d2a614704b26b1f3b88d665820
Esgawd9 <zsombor.pinter0105@gmail.com>
2025-12-20 14:21:40 +0100

    added Open Graph image

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -3,24 +3,32 @@ import React from "react";
 const SEO = ({ title, description, path }) => {
   const domain = "https://esgawd9-portfolio.vercel.app";
   const fullUrl = path ? `${domain}${path}` : domain;
+  
+  // Open Graph image URL
+  const imageUrl = `${domain}/og-image.png`;
 
   return (
     <>
-      {/* better than react-helmet-async */}
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="canonical" href={fullUrl} />
 
-      {/* Open Graph */}
+      {/* Open Graph (Facebook, LinkedIn) */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={fullUrl} />
       <meta property="og:type" content="website" />
+      
+      {/* og: image: */}
+      <meta property="og:image" content={imageUrl} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
 
-      {/* Twitter */}
+      {/* Twitter Card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={imageUrl} />
     </>
   );
 };
